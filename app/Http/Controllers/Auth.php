7aaad7cc997d00fd6ed5aa0ce414af9e32529115
hovuanhkhoa <hovuanhkhoa@gmail.com>
2017-01-03 20:56:04 +0700

    Register and Refreshtoken

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -6,7 +6,10 @@ use App\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+
 
 class Auth extends Controller
 {
@@ -16,9 +19,10 @@ class Auth extends Controller
             && $request->has('client_id')
             && $request->has('client_secret')
             && $request->has('username')
-            && $request->has('password')){
+            && $request->has('password')
+        ){
 
-            $role = CheckRole($request->get('username'));
+            $role = $this->CheckRole($request->get('username'));
 
             if($role === 'admin') {
                 $request->request->add(['scope' => 'admin']);
@@ -38,12 +42,78 @@ class Auth extends Controller
             return  Route::dispatch($tokenRequest);
         }
         else{
-            return json_encode('parameter is not correct');
+            return response()->json([
+                'message' => 'Parameter is wrong'
+            ], 400);
         }
     }
 
-    public function CheckRole($email)
-    {
+
+    public function RefreshToken(Request $request){
+        if($request->has('grant_type')
+            && $request->has('client_id')
+            && $request->has('client_secret')
+            && $request->has('refresh_token')
+            && $request->has('username')
+        ){
+
+            $role = $this->CheckRole($request->get('username'));
+
+            if($role === 'admin') {
+                $request->request->add(['scope' => 'admin']);
+            }
+            else if ($role === 'user'){
+                $request->request->add(['scope' => 'user']);
+            }
+            else{
+                $request->request->add(['scope' => 'guest']);
+            }
+
+
+            $tokenRequest = Request::create(
+                '/oauth/token',
+                'post'
+            );
+
+            return  Route::dispatch($tokenRequest);
+        }
+        else{
+            return response()->json([
+                'message' => 'Parameter is wrong'
+            ], 400);
+        }
+    }
+
+    public function Register(Request $request){
+        if($request->has('username')
+            && $request->has('password')
+            && $request->has('name')
+        ){
+            $user = new User;
+            $user->id = User::count() + 1 ;
+            $user->name = $request->get('name');
+            $user->email = $request->get('username');
+            $user->password = Hash::make($request->get('password'));
+
+            if(User::where('email',$user->email)->count() > 0)
+                return response()->json([
+                    'message' => 'Email is existed'
+                ], 403);
+
+            $user->save();
+            return  response()->json([
+                'username' => $user->email
+            ], 200);
+        }
+        else{
+            return response()->json([
+                'message' => 'Parameter is wrong'
+            ], 400);
+        }
+    }
+
+
+    public function CheckRole($email){
         $role =  DB::table('users')
             ->join('roles','users.role','roles.id')
             ->select('roles.name as name')
@@ -51,6 +121,9 @@ class Auth extends Controller
             ->first();
         if($role === null){
             $role = 'guest';
+        }
+        else{
+            $role = $role->name;
         }
         return $role;
     }
