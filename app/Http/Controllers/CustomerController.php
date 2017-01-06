24653c00e7b17f5d2409a79df5ef8679ac77d535
hovuanhkhoa <hovuanhkhoa@gmail.com>
2017-01-06 17:44:23 +0700

    Get user's info of token

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Auth;
+
+class CustomerController extends Controller
+{
+    //
+    public function index(Request $request){
+        $user = $request->user();
+        $me = User::select('users.ID as id',
+            'Username as username',
+            'Customer_name as customerName',
+            'Email as email',
+            'Identify_number as identifyNumber',
+            'Phone as phone',
+            'Address as address'
+            )->where('users.ID',$user->ID)
+            ->join('customers','customers.ID','users.ID_CUSTOMER')
+            ->first();
+
+        return $me;
+    }
+}
